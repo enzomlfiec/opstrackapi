8f86616a8ec86a9df1c25b0d52cb78499af4bfc9
Enzo Meneghin <enzo41935@fiec.edu.br>
2026-08-17 09:52:53 -0300

    feat: adicionado a pagina welcome na rota raiz

--- app.py
+++ app.py
@@ -2,9 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Rota Raiz.
+
 @app.route('/')
+def welcome():
+    return "<h1>Bem vindo!</h1>"
+
+# Rota Raiz.
+
+@app.route('/status')
 def status():
     return {'servico': 'OpsTrackAPI','status': 'online'}
+
+# Rota Raiz.
+
+@app.route('/tickets')
+def tickets():
+    return {'servico': 'OpsTrackAPI','status': 'online'}
+
+# Rota Raiz.
+
+@app.route('/sobre')
+def sobre():
+    return {'servico': 'OpsTrackAPI','status': 'online'}
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
